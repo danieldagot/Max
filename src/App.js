9ff68434f9b609fd37eb660d71488c6b076f1a58
danieldagot <danieldagot@gmail.com>
2019-10-24 18:07:12 +0300

    modal

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from "react";
 import logo from './logo.svg';
+import Modal from 'react-bootstrap/Modal' 
+ import{ ButtonToolbar ,Button}  from 'react-bootstrap' 
 import './App.css';
 import { BrowserRouter as Router, Route, Link, Redirect } from 'react-router-dom'
 import Workouts from "./Components/Workouts"
@@ -16,10 +18,13 @@ let human ={
   sex : "male" , 
   activity : 1 
 }
+
 let L = new Logic(human.hight,human.weight,human.age,human.sex,human.activity)
 localStorage.setItem("human",JSON.stringify(L))
 function App() {
   console.log(L);
+  const [smShow, setSmShow] = useState(false);
+  const [lgShow, setLgShow] = useState(false);
   return (
 
     <Router>
@@ -30,7 +35,24 @@ function App() {
       
     
         <div className=" camorshal" >
-         
+        <ButtonToolbar>
+      <Button onClick={() => setLgShow(true)}>Large modal</Button>
+
+  
+      <Modal
+        size="lg"
+        show={lgShow}
+        onHide={() => setLgShow(false)}
+        aria-labelledby="example-modal-sizes-title-lg"
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-lg">
+            Bmi 
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body> <Bmi /> </Modal.Body>
+      </Modal>
+    </ButtonToolbar>
       </div>
       </div>
 
